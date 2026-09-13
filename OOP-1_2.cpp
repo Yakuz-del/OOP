ae7f8c045ca09e568650a70380f13c05c5ce034d
Yakuz-del <yurashivan17@gmail.com>
2026-09-13 23:15:13 +0300

    Add files via upload

---
+++ OOP-1_2.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Figure {
+public:
+    virtual double area() = 0;        
+    virtual double perimeter() = 0;   
+    virtual ~Figure() {}              
+};
+
+class Circle : public Figure {
+private:
+    double radius;
+
+public:
+    Circle(double radius) : radius(radius) {}
+
+    double area() override {
+        return 3.14 * radius * radius;
+    }
+
+    double perimeter() override {
+        return 2 * 3.14 * radius;
+    }
+};
+
+class Square : public Figure {
+private:
+    double side;
+
+public:
+    Square(double side) : side(side) {}
+
+    double area() override {
+        return side * side;
+    }
+
+    double perimeter() override {
+        return 4 * side;
+    }
+};
+
+class Rectangle : public Figure {
+private:
+    double width;
+    double height;
+
+public:
+    Rectangle(double width, double height)
+        : width(width), height(height) {
+    }
+
+    double area() override {
+        return width * height;
+    }
+
+    double perimeter() override {
+        return 2 * (width + height);
+    }
+};
+
+int main() {
+    Circle circle(5);
+    Square square(4);
+    Rectangle rectangle(3, 6);
+
+    cout << "Circle: area = " << circle.area()
+        << ", perimeter = " << circle.perimeter() << endl;
+
+    cout << "Square: area = " << square.area()
+        << ", perimeter = " << square.perimeter() << endl;
+
+    cout << "Rectangle: area = " << rectangle.area()
+        << ", perimeter = " << rectangle.perimeter() << endl;
+
+    return 0;
+}
