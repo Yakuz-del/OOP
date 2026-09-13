4ee2dba2667a166355d30b3fa143a777a5a57d4a
Yakuz-del <yurashivan17@gmail.com>
2026-09-13 23:48:41 +0300

    Add files via upload

---
+++ OPP-1_4.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+using namespace std;
+
+class Vector {
+private:
+    double x, y, z;
+
+public:
+ 
+    Vector() : x(0), y(0), z(0) {}
+
+
+    Vector(double x, double y, double z) : x(x), y(y), z(z) {}
+
+ 
+    Vector add(const Vector& other) const {
+        return Vector(x + other.x, y + other.y, z + other.z);
+    }
+
+
+    Vector subtract(const Vector& other) const {
+        return Vector(x - other.x, y - other.y, z - other.z);
+    }
+
+    Vector multiply(double k) const {
+        return Vector(x * k, y * k, z * k);
+    }
+
+
+    Vector divide(double k) const {
+        return Vector(x / k, y / k, z / k);
+    }
+
+ 
+    void print() const {
+        cout << "(" << x << ", " << y << ", " << z << ")" << endl;
+    }
+};
+
+int main() {
+    Vector a(1, 2, 3);
+    Vector b(4, 5, 6);
+    Vector zero;
+
+    cout << "a = ";         a.print();
+    cout << "b = ";         b.print();
+    cout << "zero = ";      zero.print();
+
+    cout << "a + b = ";     a.add(b).print();
+    cout << "a - b = ";     a.subtract(b).print();
+    cout << "a * 2 = ";     a.multiply(2).print();
+    cout << "a / 2 = ";     a.divide(2).print();
+
+    return 0;
+}
